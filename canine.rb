93d4a0a0cb7dd1418ecb1cf402f5b4acf8743f1b
Max Jacobson <max@hardscrabble.net>
2016-11-10 23:14:51 -0500

    Implement dog years formula
    According to the internet, this is how you measure a dog's years:
    
    take their age in _human years_ and then ... well, if they're less than
    3 years old, just multiply by 10.5
    
    so a 2 year old dog is kind of like a 21 year old
    
    don't feed your dog beer though, probably
    
    later on it gets a little more complicated
    
    The whole thing is very real and important and not a way to help cope
    with the crush of mortality

--- canine.rb
+++ canine.rb
@@ -1,2 +1,17 @@
 class Canine
+  def initialize(human_years)
+    @human_years = human_years
+  end
+
+  def age
+    case human_years
+    when 0..2 then human_years * 10.5
+    else
+      21 + (human_years - 2) * 4
+    end
+  end
+
+  private
+
+  attr_reader :human_years
 end
