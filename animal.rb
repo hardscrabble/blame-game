c8ffb549be17c6deacdb4c438807f4de02883df3
Max Jacobson <max@hardscrabble.net>
2016-11-10 23:18:06 -0500

    Extract Animal base class
    Could come in handy!!

---
+++ animal.rb
@@ -0,0 +1,9 @@
+class Animal
+  def initialize(human_years)
+    @human_years = human_years
+  end
+
+  private
+
+  attr_reader :human_years
+end
--- canine.rb
+++ canine.rb
@@ -1,8 +1,6 @@
-class Canine
-  def initialize(human_years)
-    @human_years = human_years
-  end
+require "./animal"
 
+class Canine < Animal
   def age
     case human_years
     when 0..2 then human_years * 10.5
@@ -10,8 +8,6 @@ class Canine
       21 + (human_years - 2) * 4
     end
   end
-
-  private
-
-  attr_reader :human_years
 end
+
+puts Canine.new(5).age
