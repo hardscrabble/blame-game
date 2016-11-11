4c5e50a9c14560d3ef6bb777fafa81f89a1cb848
Max Jacobson <max@hardscrabble.net>
2016-11-10 23:28:27 -0500

    Extract some magic numbers

--- age.rb
+++ age.rb
@@ -1,9 +1,13 @@
 module Age
+  BASE_AGE = 21
+  CHILD_MULTIPLIER = 10.5
+  ADULTHOOD_OFFSET = 2
+  ADULTHOOD_MULTIPLIER = 4
   def age
     if (0..2).cover?(human_years)
-      human_years * 10.5
+      human_years * CHILD_MULTIPLIER
     else
-      21 + (human_years - 2) * 4
+      BASE_AGE + (human_years - ADULTHOOD_OFFSET) * ADULTHOOD_MULTIPLIER
     end
   end
 end
--- dog.rb
+++ dog.rb
@@ -5,4 +5,9 @@ class Dog < Animal
   include Age
 end
 
+puts Dog.new(0).age
+puts Dog.new(1).age
+puts Dog.new(2).age
+puts Dog.new(3).age
+puts Dog.new(4).age
 puts Dog.new(5).age
